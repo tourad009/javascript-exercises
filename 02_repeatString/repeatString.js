57d75c89cf05d6163f97b44c9e9ae96565a54acb
Tourad <tourad959@gmail.com>
2024-08-08 06:57:19 +0000

    All tests passed for exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count === 0 || str === '') {
+        return '';
+    } else if (count > 0){
+        return str.repeat(count);
+    } else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
