e16eb1629f3cbcf9cc2518d7835153464f170e9f
Tourad <tourad959@gmail.com>
2024-08-08 20:58:14 +0000

    All tests passed for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    const array = [];
 
-};
+    if (typeof start === "string" || typeof end === "string") {
+        return 'ERROR';
+    }
+
+    if (isNaN(start) || isNaN(end)) {
+        return 'ERROR';
+    }
+
+    const min = Math.min(start, end);
+    const max = Math.max(start, end);
+
+    if (min < 0 || max < 0) {
+        return 'ERROR';
+    }
+
+    if (Number.isInteger(min) === true && Number.isInteger(max) === true) {
+        for (let i = min; i <= max; i++) {
+            array.push(i);
+        }
+        return array.reduce((acc, value) => acc + value, 0);
+    } else {
+        return 'ERROR';
+    }
+}; 
 
 // Do not edit below this line
 module.exports = sumAll;
