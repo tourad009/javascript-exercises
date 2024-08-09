d0a833a528b8ec8489406dc5e4f8df799c149ee7
Tourad <tourad959@gmail.com>
2024-08-09 06:48:30 +0000

    All tests passed for exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  const celsiusTemp = (5 / 9) * (fahrenheitTemp - 32);
+  return Number(celsiusTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const fahrenheitTemp = (9 / 5) * celsiusTemp + 32;
+  return Number(fahrenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line
